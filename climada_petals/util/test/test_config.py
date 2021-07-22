23c9479753cbdc6a76733512adfddcdd4f7fbcc5
schmide <emanuel.schmid@id.ethz.ch>
2021-07-22 17:58:47 +0200

    allow constants to be distributed into core and petals

--- climada_petals/util/test/test_config.py
+++ climada_petals/util/test/test_config.py
@@ -19,8 +19,10 @@ with CLIMADA. If not, see <https://www.gnu.org/licenses/>.
 Test config module.
 """
 import unittest
+from pathlib import Path
 
-from climada.util.config import Config, CONFIG
+from climada.util.constants import DEMO_DIR
+from climada.util.config import CONFIG
 
 class TestConfig(unittest.TestCase):
     """Test Config methods"""
@@ -31,35 +33,10 @@ class TestConfig(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_from_dict(self):
-        """Check the creation and use of a Config object."""
-        dct = {'a': 4.,
-               'b': [0, 1., '2', {'c': 'c'}, [[11, 12], [21, 22]]]}
-        conf = Config.from_dict(dct)
-        self.assertEqual(conf.a.float(), 4.0)
-        self.assertEqual(str(conf), '{a: 4.0, b: [0, 1.0, 2, {c: c}, [[11, 12], [21, 22]]]}')
-        try:
-            conf.a.int()
-            self.fail("this should have failed with `<class 'float'>, not int`")
-        except:
-            pass
-        self.assertEqual(conf.b.get(0).int(), 0)
-        self.assertEqual(conf.b.int(0), 0)
-        self.assertEqual(conf.b.float(1), 1.)
-        self.assertEqual(conf.b.str(2), '2')
-        self.assertEqual(conf.b.get(3).c.str(), 'c')
-        self.assertEqual(conf.b.get(4, 1, 0).int(), 21)
-        self.assertEqual(conf.b.get(4, 1).int(1), 22)
-        self.assertEqual(conf.b.get(4).list(0)[1].int(), 12)
-        self.assertEqual(conf.b.list(4)[0].int(0), 11)
-
-    def test_substitute(self):
-        global CONFIG
-        """Check the substitution of references."""
-        dct = {'a': 'https://{b.c}/{b.d}.{b.e}', 'b': {'c': 'host', 'd': 'page', 'e': 'domain'}}
-        conf = Config.from_dict(dct)
-        self.assertEqual(conf.a._root, conf._root)
-        self.assertEqual(conf.a.str(), 'https://host/page.domain')
+    def test_check_constants_depending_on_config(self):
+        """Check whether the petals configuration correctly supersedes."""
+        self.assertEqual(CONFIG.local_data.demo.str(), './data/demo')
+        self.assertEqual(DEMO_DIR, Path('./data/demo').absolute())
 
 
 # Execute Tests
