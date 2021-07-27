a14b0402911801590c1b09b33a26f931ce4d8680
Thomas Vogt <thomas.vogt@pik-potsdam.de>
2021-07-27 13:56:22 +0200

    Repository split

--- setup.py
+++ setup.py
@@ -10,21 +10,13 @@ here = Path(__file__).parent.absolute()
 with open(here.joinpath('README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# Get the data recursively from the data folder
-def package_files(directory):
-    return [str(path_object)
-        for path_object in directory.glob('**/*')
-        if path_object.is_file() and path_object.name[0] != '.'
-    ]
-
-extra_files = package_files(here / 'data')
 # Add configuration files
-extra_files.append(str(here / 'climada/conf/climada.conf'))
+extra_files = [str(here / 'climada_petals/conf/climada.conf')]
 
 setup(
-    name='climada',
+    name='climada_petals',
 
-    version='2.1.1',
+    version='3.0.0-cand',
 
     description='CLIMADA in Python',
 
@@ -51,6 +43,7 @@ setup(
 
     install_requires=[
         'bottleneck',
+        'climada',
         'cartopy',
         'cfgrib',
         'contextily',
