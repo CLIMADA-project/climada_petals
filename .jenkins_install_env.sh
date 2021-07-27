e66d94710a8c9bfa2ec3c1e8bcf507157eb18443
schmide <emanuel.schmid@id.ethz.ch>
2021-07-27 21:10:11 +0200

    petals environment installation

--- .jenkins_install_env.sh
+++ .jenkins_install_env.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -e
 
-conda remove --name climada_env --all
-conda env create -f requirements/env_climada.yml --name climada_env
+source activate
 
-source activate climada_env
+mamba env remove -n petals_env
+mamba env create -n petals_env -f requirements/env_climada.yml
+
+conda activate petals_env
+pip install -e ~/jobs/climada_install_env/workspace
+
 make install_test
 conda deactivate
