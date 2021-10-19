658a63c11f87754da07c27699cc894d1b0d513ee
Thomas Vogt <thomas.vogt@pik-potsdam.de>
2021-10-19 14:51:53 +0200

    ImpfRiverFlood: set_RF_Impf_* -> from_region

--- climada_petals/entity/impact_funcs/river_flood.py
+++ climada_petals/entity/impact_funcs/river_flood.py
@@ -55,239 +55,114 @@ class ImpfRiverFlood(ImpactFunc):
         self.continent = ''
 
     @classmethod
-    def from_RF_Impf_Africa(cls):
-        """
-        
+    def from_region(cls, region):
+        """Create a new ImpfRiverFlood object with parameters for the specified world region.
 
         Parameters
         ----------
-        cls : TYPE
-            DESCRIPTION.
+        region : str
+            Use damage function parameters for this world region. Supported values: "Africa",
+            "Asia", "Europe", "NorthAmerica", "Oceania", "SouthAmerica".
 
         Returns
         -------
-        Impf : TYPE
-            DESCRIPTION.
-
+        impf : ImpfRiverFlood
+            New ImpfRiverFlood object with parameters for the specified world region.
         """
-        Impf = cls()
-        Impf.id = 1
-        Impf.name = "Flood Africa JRC Residential noPAA"
-        Impf.continent = 'Africa'
-        Impf.intensity = np.array([0., 0.5, 1., 1.5, 2., 3., 4., 5., 6., 12.])
-        Impf.mdd = np.array([0.0000, 0.2199, 0.3782, 0.5306, 0.6356, 0.8169,
-                             0.9034, 0.9572, 1.0000, 1.0000])
-
-        Impf.mdr = np.array([0.0000, 0.2199, 0.3782, 0.5306, 0.6356, 0.8169,
-                             0.9034, 0.9572, 1.0000, 1.0000])
-
-        Impf.paa = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-        
-        return Impf
+        impf = cls()
+        if region.lower() == "africa":
+            impf.id = 1
+            impf.name = "Flood Africa JRC Residential noPAA"
+            impf.continent = 'Africa'
+            impf.intensity = np.array([0., 0.5, 1., 1.5, 2., 3., 4., 5., 6., 12.])
+            impf.mdd = np.array([0.0000, 0.2199, 0.3782, 0.5306, 0.6356, 0.8169,
+                                 0.9034, 0.9572, 1.0000, 1.0000])
+            impf.mdr = np.array([0.0000, 0.2199, 0.3782, 0.5306, 0.6356, 0.8169,
+                                 0.9034, 0.9572, 1.0000, 1.0000])
+        elif region.lower() == "asia":
+            impf.id = 2
+            impf.name = "Flood Asia JRC Residential noPAA"
+            impf.continent = 'Asia'
+            impf.intensity = np.array([0., 0.5, 1., 1.5, 2., 3., 4., 5., 6., 12.])
+            impf.mdd = np.array([0.000, 0.3266, 0.4941, 0.6166, 0.7207, 0.8695,
+                                 0.9315, 0.9836, 1.0000, 1.0000])
+            impf.mdr = np.array([0.000, 0.3266, 0.4941, 0.6166, 0.7207, 0.8695,
+                                 0.9315, 0.9836, 1.0000, 1.0000])
+        elif region.lower() == "europe":
+            impf.id = 3
+            impf.name = "Flood Europe JRC Residential noPAA"
+            impf.continent = 'Europe'
+            impf.intensity = np.array([0., 0.5, 1., 1.5, 2., 3., 4., 5., 6., 12.])
+            impf.mdd = np.array([0.00, 0.25, 0.40, 0.50, 0.60, 0.75, 0.85, 0.95,
+                                 1.00, 1.00])
+            impf.mdr = np.array([0.000, 0.250, 0.400, 0.500, 0.600, 0.750, 0.850,
+                                 0.950, 1.000, 1.000])
+        elif region.lower().replace(" ", "") == "northamerica":
+            impf.id = 4
+            impf.name = "Flood North America JRC Residential noPAA"
+            impf.continent = 'NorthAmerica'
+            impf.intensity = np.array([0., 0.1, 0.5, 1., 1.5, 2., 3., 4., 5.,
+                                      6., 12.])
+            impf.mdd = np.array([0.0000, 0.2018, 0.4433, 0.5828, 0.6825, 0.7840,
+                                 0.8543, 0.9237, 0.9585, 1.0000, 1.0000])
+            impf.mdr = np.array([0.0000, 0.2018, 0.4433, 0.5828, 0.6825, 0.7840,
+                                 0.8543, 0.9237, 0.9585, 1.0000, 1.0000])
+        elif region.lower() == "oceania":
+            impf.id = 5
+            impf.name = "Flood Oceania JRC Residential noPAA"
+            impf.continent = 'Oceania'
+            impf.intensity =  np.array([0., 0.5, 1., 1.5, 2., 3., 4., 5., 6., 12.])
+            impf.mdd = np.array([0.00, 0.48, 0.64, 0.71, 0.79, 0.93, 0.97, 0.98,
+                                 1.00, 1.00])
+            impf.mdr = np.array([0.000, 0.480, 0.640, 0.710, 0.790, 0.930, 0.970,
+                                 0.980, 1.000, 1.000])
+        elif region.lower().replace(" ", "") == "southamerica":
+            impf.id = 6
+            impf.name = "Flood South America JRC Residential noPAA"
+            impf.continent = 'SouthAmerica'
+            impf.intensity =  np.array([0., 0.5, 1., 1.5, 2., 3., 4., 5., 6., 12.])
+            impf.mdd = np.array([0.0000, 0.4908, 0.7112, 0.8420, 0.9494,
+                                 0.9836, 1.0000, 1.0000, 1.0000, 1.0000])
+            impf.mdr = np.array([0.0000, 0.4908, 0.7112, 0.8420, 0.9494, 0.9836,
+                                 1.0000, 1.0000, 1.0000, 1.0000])
+        impf.paa = np.ones(len(impf.intensity))
+        return impf
 
     def set_RF_Impf_Africa(self, *args, **kwargs):
-        """This function is deprecated, use ImpfRiverFlood.from_RF_Impf_Africa instead."""
-        LOGGER.warning("The use of ImpfRiverFlood().set_RF_Impf_Africa is deprecated."
-                       "Use ImpfRiverFlood.from_RF_Impf_Africa instead.")
-        self.__dict__ = ImpfRiverFlood.from_RF_Impf_Africa(*args, **kwargs).__dict__
-        
-
-        
-    @classmethod
-    def from_RF_Impf_Asia(cls):
-        """
-        
-
-        Parameters
-        ----------
-        cls : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        Impf : TYPE
-            DESCRIPTION.
-
-        """
-        Impf = cls()
-        Impf.id = 2
-        Impf.name = "Flood Asia JRC Residential noPAA"
-        Impf.continent = 'Asia'
-        Impf.intensity = np.array([0., 0.5, 1., 1.5, 2., 3., 4., 5., 6., 12.])
-        Impf.mdd = np.array([0.000, 0.3266, 0.4941, 0.6166, 0.7207, 0.8695,
-                             0.9315, 0.9836, 1.0000, 1.0000])
-
-        Impf.mdr = np.array([0.000, 0.3266, 0.4941, 0.6166, 0.7207, 0.8695,
-                             0.9315, 0.9836, 1.0000, 1.0000])
-
-        Impf.paa = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-        
-        return Impf
+        """This function is deprecated, use ImpfRiverFlood.from_region instead."""
+        LOGGER.warning("The use of ImpfRiverFlood.set_RF_Impf_Africa is deprecated."
+                       "Use ImpfRiverFlood.from_region instead.")
+        self.__dict__ = ImpfRiverFlood.from_region("Africa", *args, **kwargs).__dict__
 
     def set_RF_Impf_Asia(self, *args, **kwargs):
-        """This function is deprecated, use ImpfRiverFlood.from_RF_Impf_Asia instead."""
-        LOGGER.warning("The use of ImpfRiverFlood().set_RF_Impf_Asia is deprecated."
-                       "Use ImpfRiverFlood.from_RF_Impf_Asia instead.")
-        self.__dict__ = ImpfRiverFlood.from_RF_Impf_Asia(*args, **kwargs).__dict__
-        
-
-    @classmethod
-    def from_RF_Impf_Europe(cls):
-        """
-        
-
-        Parameters
-        ----------
-        cls : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        Impf : TYPE
-            DESCRIPTION.
-
-        """
-        Impf = cls()
-        Impf.id = 3
-        Impf.name = "Flood Europe JRC Residential noPAA"
-        Impf.continent = 'Europe'
-        Impf.intensity = np.array([0., 0.5, 1., 1.5, 2., 3., 4., 5., 6., 12.])
-        Impf.mdd = np.array([0.00, 0.25, 0.40, 0.50, 0.60, 0.75, 0.85, 0.95,
-                             1.00, 1.00])
-
-        Impf.mdr = np.array([0.000, 0.250, 0.400, 0.500, 0.600, 0.750, 0.850,
-                             0.950, 1.000, 1.000])
-
-        Impf.paa = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-        
-        return Impf
+        """This function is deprecated, use ImpfRiverFlood.from_region instead."""
+        LOGGER.warning("The use of ImpfRiverFlood.set_RF_Impf_Asia is deprecated."
+                       "Use ImpfRiverFlood.from_region instead.")
+        self.__dict__ = ImpfRiverFlood.from_region("Asia", *args, **kwargs).__dict__
 
     def set_RF_Impf_Europe(self, *args, **kwargs):
-        """This function is deprecated, use ImpfRiverFlood.from_RF_Impf_Europe instead."""
-        
-        LOGGER.warning("The use of ImpfRiverFlood().set_RF_Impf_Europe is deprecated."
-                       "Use ImpfRiverFlood.from_RF_Impf_Europe instead.")
-        self.__dict__ = ImpfRiverFlood.from_RF_Impf_Europe(*args, **kwargs).__dict__
-        
-
-    @classmethod
-    def from_RF_Impf_NorthAmerica(cls):
-        """
-        
-
-        Parameters
-        ----------
-        cls : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        Impf : TYPE
-            DESCRIPTION.
-
-        """
-        Impf = cls()
-        Impf.id = 4
-        Impf.name = "Flood North America JRC Residential noPAA"
-        Impf.continent = 'NorthAmerica'
-        Impf.intensity = np.array([0., 0.1, 0.5, 1., 1.5, 2., 3., 4., 5.,
-                                  6., 12.])
-    
-        Impf.mdd = np.array([0.0000, 0.2018, 0.4433, 0.5828, 0.6825, 0.7840,
-                             0.8543, 0.9237, 0.9585, 1.0000, 1.0000])
-
-        Impf.mdr = np.array([0.0000, 0.2018, 0.4433, 0.5828, 0.6825, 0.7840,
-                             0.8543, 0.9237, 0.9585, 1.0000, 1.0000])
-
-        Impf.paa = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-        
-        return Impf
+        """This function is deprecated, use ImpfRiverFlood.from_region instead."""
+        LOGGER.warning("The use of ImpfRiverFlood.set_RF_Impf_Europe is deprecated."
+                       "Use ImpfRiverFlood.from_region instead.")
+        self.__dict__ = ImpfRiverFlood.from_region("Europe", *args, **kwargs).__dict__
 
     def set_RF_Impf_NorthAmerica(self, *args, **kwargs):
-        """This function is deprecated, use ImpfRiverFlood.from_RF_Impf_NorthAmerica instead."""
-        
-        LOGGER.warning("The use of ImpfRiverFlood().set_RF_Impf_NorthAmerica is deprecated."
-                       "Use ImpfRiverFlood.from_RF_Impf_NorthAmerica instead.")
-        self.__dict__ = ImpfRiverFlood.from_RF_Impf_NorthAmerica(*args, **kwargs).__dict__
-
-
-    @classmethod
-    def from_RF_Impf_Oceania(cls):
-        """
-        
-
-        Parameters
-        ----------
-        cls : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        Impf : TYPE
-            DESCRIPTION.
-
-        """
-        Impf = cls()
-        Impf.id = 5
-        Impf.name = "Flood Oceania JRC Residential noPAA"
-        Impf.continent = 'Oceania'
-        Impf.intensity =  np.array([0., 0.5, 1., 1.5, 2., 3., 4., 5., 6., 12.])
-    
-        Impf.mdd = np.array([0.00, 0.48, 0.64, 0.71, 0.79, 0.93, 0.97, 0.98,
-                             1.00, 1.00])
-
-        Impf.mdr = np.array([0.000, 0.480, 0.640, 0.710, 0.790, 0.930, 0.970,
-                             0.980, 1.000, 1.000])
-
-        Impf.paa = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-        
-        return Impf
+        """This function is deprecated, use ImpfRiverFlood.from_region instead."""
+        LOGGER.warning("The use of ImpfRiverFlood.set_RF_Impf_NorthAmerica is deprecated."
+                       "Use ImpfRiverFlood.from_region instead.")
+        self.__dict__ = ImpfRiverFlood.from_region("NorthAmerica", *args, **kwargs).__dict__
 
     def set_RF_Impf_Oceania(self, *args, **kwargs):
-        """This function is deprecated, use ImpfRiverFlood.from_RF_Impf_Oceania instead."""
-        
-        LOGGER.warning("The use of ImpfRiverFlood().set_RF_Impf_Oceania is deprecated."
-                       "Use ImpfRiverFlood.from_RF_Impf_Oceania instead.")
-        self.__dict__ = ImpfRiverFlood.from_RF_Impf_Oceania(*args, **kwargs).__dict__
-
-    @classmethod
-    def from_RF_Impf_SouthAmerica(cls):
-        """
-        
-
-        Parameters
-        ----------
-        cls : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        Impf : TYPE
-            DESCRIPTION.
-
-        """
-        Impf = cls()
-        Impf.id = 6
-        Impf.name = "Flood South America JRC Residential noPAA"
-        Impf.continent = 'SouthAmerica'
-        Impf.intensity =  np.array([0., 0.5, 1., 1.5, 2., 3., 4., 5., 6., 12.])
-    
-        Impf.mdd = np.array([0.0000, 0.4908, 0.7112, 0.8420, 0.9494,
-                             0.9836, 1.0000, 1.0000, 1.0000, 1.0000])
-
-        Impf.mdr = np.array([0.0000, 0.4908, 0.7112, 0.8420, 0.9494, 0.9836,
-                             1.0000, 1.0000, 1.0000, 1.0000])
-
-        Impf.paa = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-        
-        return Impf
+        """This function is deprecated, use ImpfRiverFlood.from_region instead."""
+        LOGGER.warning("The use of ImpfRiverFlood.set_RF_Impf_Oceania is deprecated."
+                       "Use ImpfRiverFlood.from_region instead.")
+        self.__dict__ = ImpfRiverFlood.from_region("Oceania", *args, **kwargs).__dict__
 
     def set_RF_Impf_SouthAmerica(self, *args, **kwargs):
-        """This function is deprecated, use ImpfRiverFlood.from_RF_Impf_SouthAmerica instead."""
-        
-        LOGGER.warning("The use of ImpfRiverFlood().set_RF_Impf_SouthAmerica is deprecated."
-                       "Use ImpfRiverFlood.from_RF_Impf_SouthAmerica instead.")
-        self.__dict__ = ImpfRiverFlood.from_RF_Impf_SouthAmerica(*args, **kwargs).__dict__
+        """This function is deprecated, use ImpfRiverFlood.from_region instead."""
+        LOGGER.warning("The use of ImpfRiverFlood.set_RF_Impf_SouthAmerica is deprecated."
+                       "Use ImpfRiverFlood.from_region instead.")
+        self.__dict__ = ImpfRiverFlood.from_region("SouthAmerica", *args, **kwargs).__dict__
 
 
 def flood_imp_func_set():
@@ -295,22 +170,22 @@ def flood_imp_func_set():
 
     impf_set = ImpactFuncSet()
 
-    impf_africa = ImpfRiverFlood.from_RF_Impf_Africa()
+    impf_africa = ImpfRiverFlood.from_region("Africa")
     impf_set.append(impf_africa)
 
-    impf_asia = ImpfRiverFlood.from_RF_Impf_Asia()
+    impf_asia = ImpfRiverFlood.from_region("Asia")
     impf_set.append(impf_asia)
 
-    impf_europe = ImpfRiverFlood.from_RF_Impf_Europe()
+    impf_europe = ImpfRiverFlood.from_region("Europe")
     impf_set.append(impf_europe)
 
-    impf_na = ImpfRiverFlood.from_RF_Impf_NorthAmerica()
+    impf_na = ImpfRiverFlood.from_region("NorthAmerica")
     impf_set.append(impf_na)
 
-    impf_oceania = ImpfRiverFlood.from_RF_Impf_Oceania()
+    impf_oceania = ImpfRiverFlood.from_region("Oceania")
     impf_set.append(impf_oceania)
 
-    impf_sa = ImpfRiverFlood.from_RF_Impf_SouthAmerica()
+    impf_sa = ImpfRiverFlood.from_region("SouthAmerica")
     impf_set.append(impf_sa)
 
     return impf_set
--- climada_petals/entity/impact_funcs/test/test_fl.py
+++ climada_petals/entity/impact_funcs/test/test_fl.py
@@ -33,9 +33,9 @@ class TestIFRiverFlood(unittest.TestCase):
                         np.array(['RF'])))
         self.assertEqual(test_set.size(), 6)
 
-    def test_from_RF_impf_Africa(self):
+    def test_region_Africa(self):
 
-        impf_1 = fl.ImpfRiverFlood.from_RF_Impf_Africa()
+        impf_1 = fl.ImpfRiverFlood.from_region("Africa")
 
         self.assertEqual(impf_1.continent, 'Africa')
         self.assertEqual(impf_1.name, 'Flood Africa JRC Residential noPAA')
@@ -52,10 +52,10 @@ class TestIFRiverFlood(unittest.TestCase):
         self.assertTrue(np.allclose(impf_1.paa,
                         np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])))
 
-    def test_from_RF_impf_Asia(self):
-        
-        impf_2 = fl.ImpfRiverFlood.from_RF_Impf_Asia()
-        
+    def test_region_Asia(self):
+
+        impf_2 = fl.ImpfRiverFlood.from_region("Asia")
+
         self.assertEqual(impf_2.continent, 'Asia')
         self.assertEqual(impf_2.name, 'Flood Asia JRC Residential noPAA')
         self.assertEqual(impf_2.haz_type, 'RF')
@@ -70,9 +70,9 @@ class TestIFRiverFlood(unittest.TestCase):
         self.assertTrue(np.allclose(impf_2.paa,
                         np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])))
 
-    def test_from_RF_impf_Europe(self):
+    def test_region_Europe(self):
 
-        impf_3 = fl.ImpfRiverFlood.from_RF_Impf_Europe()
+        impf_3 = fl.ImpfRiverFlood.from_region("Europe")
 
         self.assertEqual(impf_3.continent, 'Europe')
         self.assertEqual(impf_3.name, 'Flood Europe JRC Residential noPAA')
@@ -88,9 +88,9 @@ class TestIFRiverFlood(unittest.TestCase):
         self.assertTrue(np.allclose(impf_3.paa,
                         np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])))
 
-    def test_from_RF_impf_NorthAmerica(self):
+    def test_region_NorthAmerica(self):
 
-        impf_4 = fl.ImpfRiverFlood.from_RF_Impf_NorthAmerica()
+        impf_4 = fl.ImpfRiverFlood.from_region("NorthAmerica")
 
         self.assertEqual(impf_4.continent, 'NorthAmerica')
         self.assertEqual(impf_4.name,
@@ -101,7 +101,7 @@ class TestIFRiverFlood(unittest.TestCase):
         self.assertTrue(np.array_equal(impf_4.intensity,
                         np.array([0., 0.1, 0.5, 1., 1.5, 2., 3., 4., 5.,
                                   6., 12.])))
-        
+
         self.assertTrue(np.allclose(impf_4.mdd,
                         np.array([0.0000, 0.2018, 0.4433, 0.5828, 0.6825,
                                   0.7840, 0.8543, 0.9237, 0.9585, 1.0000,
@@ -109,9 +109,9 @@ class TestIFRiverFlood(unittest.TestCase):
         self.assertTrue(np.allclose(impf_4.paa,
                         np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])))
 
-    def test_from_RF_impf_Oceania(self):
-        
-        impf_5 = fl.ImpfRiverFlood.from_RF_Impf_Oceania()
+    def test_region_Oceania(self):
+
+        impf_5 = fl.ImpfRiverFlood.from_region("Oceania")
 
         self.assertEqual(impf_5.continent, 'Oceania')
         self.assertEqual(impf_5.name, 'Flood Oceania JRC Residential noPAA')
@@ -127,9 +127,9 @@ class TestIFRiverFlood(unittest.TestCase):
         self.assertTrue(np.allclose(impf_5.paa,
                         np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])))
 
-    def test_from_RF_impf_SouthAmerica(self):
+    def test_region_SouthAmerica(self):
 
-        impf_6 = fl.ImpfRiverFlood.from_RF_Impf_SouthAmerica()
+        impf_6 = fl.ImpfRiverFlood.from_region("SouthAmerica")
         self.assertEqual(impf_6.continent, 'SouthAmerica')
         self.assertEqual(impf_6.name,
                          'Flood South America JRC Residential noPAA')
