00e7fec8e27cbddc8397f9276bf1d6d5eb74899b
SchEma70 <schmide@ethz.ch>
2022-03-02 18:52:43 +0100

    Revert "jenkins: petals_env no need for modifiable sources"
    This reverts commit feef0a68ef851e07b9f01129107b1c70551a7f76.

--- .jenkins_install_env.sh
+++ .jenkins_install_env.sh
@@ -5,7 +5,7 @@ mamba env remove -n petals_env
 mamba env create -n petals_env -f requirements/env_climada.yml
 
 conda activate petals_env
-pip install ~/jobs/climada_install_env/workspace
+pip install -e ~/jobs/climada_install_env/workspace
 
 make install_test
 conda deactivate
