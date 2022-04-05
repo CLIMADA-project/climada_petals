dacb3e030332686260448259801fc9c7dfd513f5
Robert Blass <blassr@ethz.ch>
2022-04-05 15:36:12 +0200

    add tutorial and tests

---
+++ climada_petals/engine/test/test_warn.py
@@ -0,0 +1,109 @@
+import unittest
+import numpy as np
+
+from climada_petals.engine.warn import Warn, FilteringOrder
+
+
+class TestWarn(unittest.TestCase):
+    def test_thresholding(self):
+        thresholds = np.array([0.0, 20, 50])
+        wind_matrix = np.zeros((10, 10))
+        wind_matrix[2, 2] = -2
+        wind_matrix[3, 3] = 20
+        wind_matrix[4, 4] = 40
+        wind_matrix[5, 5] = 50
+        wind_matrix[6, 6] = 60
+
+        warn = Warn.from_np_array(wind_matrix, thresholds, operations=[], sizes=[])
+
+        self.assertEqual(warn.warning[2, 2], 0)
+        self.assertEqual(warn.warning[3, 3], 0)
+        self.assertEqual(warn.warning[4, 4], 1)
+        self.assertEqual(warn.warning[5, 5], 1)
+        self.assertEqual(warn.warning[6, 6], 2)
+        self.assertEqual(np.sum(warn.warning), 4)
+
+    def test_filtering_functionality(self):
+        thresholds = np.array([0.0, 20, 50])
+        wind_matrix = np.zeros((10, 10))
+        wind_matrix[4, 4] = 40
+
+        warn = Warn.from_np_array(wind_matrix, thresholds, operations=['DILATION'], sizes=[1])
+        self.assertEqual(np.sum(warn.warning), 5)  # 5, because of round filter shape
+        self.assertEqual(np.max(warn.warning), 1)  # 5, because of round filter shape
+
+        warn = Warn.from_np_array(wind_matrix, thresholds, operations=['EROSION'], sizes=[1])
+        self.assertEqual(np.max(warn.warning), 0)  # 5, because of round filter shape
+
+        warn = Warn.from_np_array(wind_matrix, thresholds, operations=['MEDIANFILTERING'], sizes=[2])
+        self.assertEqual(np.max(warn.warning), 0)  # 5, because of round filter shape
+
+    def test_filtering_multiple_levels(self):
+        thresholds = np.array([0.0, 20, 50, 80, 90])
+        wind_matrix = np.zeros((10, 10))
+        wind_matrix[4, 4] = 40
+        wind_matrix[7, 7] = 70
+        wind_matrix[1, 2] = 85
+
+        warn = Warn.from_np_array(wind_matrix, thresholds, operations=['DILATION'], sizes=[1])
+        self.assertEqual(np.max(warn.warning), 3)  # max shouldn't be reduced
+
+        warn = Warn.from_np_array(wind_matrix, thresholds, operations=['EROSION'], sizes=[1])
+        self.assertEqual(np.max(warn.warning), 0)  # single points reduced to level 0
+
+    def test_filtering_expand(self):
+        thresholds = np.array([0.0, 20, 50, 80])
+        wind_matrix = np.zeros((10, 10))
+        wind_matrix[6, 6] = 40
+        wind_matrix[7, 7] = 70
+
+        warn = Warn.from_np_array(wind_matrix, thresholds, expand=False, operations=['DILATION'], sizes=[1])
+        warn_exp = Warn.from_np_array(wind_matrix, thresholds, expand=True, operations=['DILATION'], sizes=[1])
+
+        # level 2 expand has no impact
+        self.assertCountEqual(warn_exp.warning[warn_exp.warning == 2], warn.warning[warn.warning == 2])
+        # expanding in level 1 is larger
+        self.assertGreater(np.count_nonzero(warn_exp.warning[warn_exp.warning == 1]),
+                           np.count_nonzero(warn.warning[warn.warning == 1]))
+        # therefore, less in level 0 for expansion
+        self.assertLess(np.count_nonzero(warn_exp.warning == 0), np.count_nonzero(warn.warning == 0))
+
+    def test_filtering_combination(self):
+        thresholds = np.array([0.0, 20, 50])
+        wind_matrix = np.zeros((10, 10))
+        wind_matrix[4, 4] = 40
+
+        # neutral operations
+        warn = Warn.from_np_array(wind_matrix, thresholds, expand=False,
+                                  operations=['DILATION', 'EROSION', 'MEDIANFILTERING'], sizes=[0, 0, 1])
+        self.assertEqual(np.sum(warn.warning), 1)
+
+        # check dilation and erosion lead to same no change in output
+        warn = Warn.from_np_array(wind_matrix, thresholds, expand=False,
+                                  operations=['DILATION', 'EROSION', 'MEDIANFILTERING'], sizes=[1, 1, 1])
+        self.assertEqual(np.sum(warn.warning), 1)
+
+    def test_small_regions(self):
+        thresholds = np.array([0.0, 20, 50])
+        wind_matrix = np.zeros((10, 10))
+        wind_matrix[4, 4] = 40
+
+        # check removal of to small region
+        warn = Warn.from_np_array(wind_matrix, thresholds, expand=False, operations=[], sizes=[])
+        warn_removed = Warn.remove_small_regions(warn.warning, 2)
+        self.assertEqual(np.sum(warn_removed), 0)
+        self.assertEqual(np.max(warn_removed), 0)
+
+        # check keeping regions
+        wind_matrix[4, 4] = 40
+        wind_matrix[4, 5] = 40
+        wind_matrix[4, 6] = 40
+        warn = Warn.from_np_array(wind_matrix, thresholds, expand=False, operations=[], sizes=[])
+        warn_kept = Warn.remove_small_regions(warn.warning, 2)
+        self.assertEqual(np.sum(warn_kept), 3)
+        self.assertEqual(np.max(warn_kept), 1)
+
+
+if __name__ == "__main__":
+    TESTS = unittest.TestLoader().loadTestsFromTestCase(TestWarn)
+    unittest.TextTestRunner(vaerbosity=2).run(TESTS)
--- climada_petals/engine/warn.py
+++ climada_petals/engine/warn.py
@@ -1,89 +1,115 @@
+import logging
 import numpy as np
-from scipy import sparse
-from scipy import ndimage
 import skimage.morphology as morph
+import skimage.filters as filters
+from skimage.measure import label
 import copy
-import cv2
+from dataclasses import dataclass
+
+LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class FilteringOrder:
+
+    def __init__(self, thresholds, expand, operations, sizes):
+        self.thresholds = thresholds
+        self.expand = expand
+        if len(operations) != len(sizes):
+            LOGGER.warning('For every operation a filter size is needed and the other way around. '
+                           'Please input the same number of operations and filter sizes.')
+        self.operations = operations
+        self.sizes = sizes
+
 
 class Warn:
+    """Explanation of defaults (also maybe transfer to config, talk to Emanuel)"""
+    operations = ['DILATION', 'EROSION', 'DILATION', 'MEDIANFILTERING']
+    sizes = [2, 3, 7, 15]
 
-    kernel_sizes = [2, 3, 7, 15]
-    """
-    kernel_sizes = [1, 3, 7, 15,
-             1, 3, 7, 15,
-             2, 3, 7, 15,
-             1, 4, 8, 15]
-    """
-    def __init__(self, thresholds, data, ):
-        self.thresholds = thresholds
-        self.data = data
+    def __init__(self, filter_data, data):
+        self.filter_data = filter_data
+        self.nr_thresholds = len(self.filter_data.thresholds) - 1
+        self.warning = np.zeros_like(data)
 
-    def thresholding(self):
-        m_thrs = np.zeros_like(self.data)
-        for i in range(1, len(self.thresholds) - 1):
-            m_thrs[self.data > self.thresholds[i]] = i
+    @classmethod
+    def from_np_array(cls, data, thresholds, expand=True, operations=operations, sizes=sizes):  # pass defaults
+        filter_data = FilteringOrder(thresholds, expand, operations, sizes)
+        warn = cls(filter_data, data)
+        d_thrs = warn.threshold_data(data)
+        warn.filter_algorithm(d_thrs)
+        return warn
+
+    def threshold_data(self, data):
+        if np.max(data) > np.max(self.filter_data.thresholds) or np.min(data) < np.min(self.filter_data.thresholds):
+            LOGGER.warning('Values of data array are smaller/larger than defined thresholds. '
+                           'Please redefine thresholds.')
+        m_thrs = np.zeros_like(data)
+        for i in range(1, len(self.filter_data.thresholds)):
+            m_thrs[data > self.filter_data.thresholds[i]] = i
         return m_thrs.astype(int)
 
-    def algo(self, m_thrs):
-        max_warn_level = m_thrs.max(axis=(0, 1))
+    def filter_algorithm(self, d_thrs):
+        def filtering(d_thrs, warn_reg, curr_lvl):
+            if self.filter_data.expand:  # select points where level is >= level under observation -> expands regions
+                pts_curr_lvl = np.bitwise_or(warn_reg, d_thrs >= curr_lvl)
+            else:  # select points where level is == level under observation -> not expanding regions
+                pts_curr_lvl = d_thrs == curr_lvl
+            reg_curr_lvl = np.where(pts_curr_lvl, curr_lvl, 0)  # set everything but pts of current level to 0
+
+            for i in range(len(self.filter_data.operations)):
+                if self.filter_data.operations[i] == 'DILATION':
+                    reg_curr_lvl = morph.dilation(reg_curr_lvl, morph.disk(self.filter_data.sizes[i]))
+                elif self.filter_data.operations[i] == 'EROSION':
+                    reg_curr_lvl = morph.erosion(reg_curr_lvl, morph.disk(self.filter_data.sizes[i]))
+                elif self.filter_data.operations[i] == 'MEDIANFILTERING':
+                    filter = np.ones((self.filter_data.sizes[i], self.filter_data.sizes[i]))
+                    reg_curr_lvl = filters.median(reg_curr_lvl, filter)
+                else:
+                    LOGGER.warning("The operation is not defined. "
+                                   "Please select 'EROSION', 'DILATION', or 'MEDIANFILTERING'.")
+            return reg_curr_lvl
+
+        max_warn_level = np.max(d_thrs)
         if max_warn_level == 0:
-            return m
+            return np.zeros(d_thrs.shape)
 
         warn_reg = 0
         cnt = 0
         # iterate from highest level to lowest (0 not necessary, because rest is level 0)
         for i in range(max_warn_level, 0, -1):
-            w_l = self.filtering(m_thrs, warn_reg, max_warn_level - i,
-                                 self.kernel_sizes[0], self.kernel_sizes[1],
-                                 self.kernel_sizes[2], self.kernel_sizes[3])
+            w_l = filtering(d_thrs, warn_reg, i)
             # keep warn regions of higher levels by taking maximum
             warn_reg = np.maximum(warn_reg, w_l)
             cnt += 4
-        return warn_reg
+        self.warning = warn_reg
 
+    @staticmethod
+    def remove_small_regions(warning, size_thr):
 
-    def filtering(self, m_thrs, warn_reg, obs, di1, er, di2, md):
-        level = np.where(np.bitwise_or(warn_reg, m_thrs >= m_thrs.max(axis=(0, 1)) - obs),
-                         m_thrs.max(axis=(0, 1)) - obs, 0)
-
-        lvl = morph.dilation(level, morph.disk(di1))
-        lvl = morph.erosion(lvl, morph.disk(er))
-        lvl = morph.dilation(lvl, morph.disk(di2))
-        lvl = ndimage.median_filter(lvl, size=md)
-        # lvl = morph.dilation(sa, morph.square(kernel_0))
-        return lvl
-
-    def remove_small_regions(self, warn_reg, size_thr):
-        max_lvl = np.max(warn_reg, axis=(0, 1))
-        min_lvl = np.min(warn_reg, axis=(0, 1))
-
-        # increase levels of too small regions to max level of this warning
-        num_labels, labels = cv2.connectedComponents(np.uint8(warn_reg))
-        for i in range(num_labels):
-            # use scipy label instead
-            cnt = np.count_nonzero(labels == i)
-            if cnt <= size_thr:
-                warn_reg[labels == i] = max_lvl
-
-        # level 0: set to small regions to max level
-        level = copy.deepcopy(warn_reg)
-        level[warn_reg == 0] = 1234
-        level[level != 1234] = 0
-        num_labels, labels = cv2.connectedComponents(np.uint8(level))
-        for j in range(num_labels):
-            cnt = np.count_nonzero(labels == j)
-            if cnt <= size_thr:
-                warn_reg[labels == j] = max_lvl
-
-        # correct the max_lvl regions generated before down,
-        # until the new regions with it are larg enough
-        for i in range(max_lvl, min_lvl, -1):
-            level = copy.deepcopy(warn_reg)
-            level[warn_reg != i] = 0
-            num_labels, labels = cv2.connectedComponents(np.uint8(level))
-            for j in range(num_labels):
-                cnt = np.count_nonzero(labels == j)
+        def increase_levels(warning, size_thr):
+            # increase levels of too small regions to max level of this warning
+            labels = label(warning)
+            for i in range(np.max(labels)):
+                cnt = np.count_nonzero(labels == i)
                 if cnt <= size_thr:
-                    warn_reg[labels == j] = i - 1
+                    warning[labels == i] = np.max(warning, axis=(0, 1))
+            return warning
 
-        return warn_reg
+        def set_new_lvl(warning, size_thr):
+            # correct the max_lvl regions generated before down,
+            # until the new regions with it are large enough
+            for i in range(np.max(warning, axis=(0, 1)), np.min(warning, axis=(0, 1)), -1):
+                level = copy.deepcopy(warning)
+                level[warning != i] = 0
+                labels = label(warning)
+                for j in range(len(np.unique(labels)) + 1):
+                    cnt = np.count_nonzero(labels == j)
+                    if cnt <= size_thr:
+                        warning[labels == j] = i - 1
+
+        warning = warning + 1  # 0 is regarded as background in labelling, + 1 prevents this
+        increase_levels(warning, size_thr)
+        set_new_lvl(warning, size_thr)
+        warning = warning - 1
+        return warning
