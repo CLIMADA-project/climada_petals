021a3bb178787166becd783911e17ba17f3f5e54
Robert Blass <blassr@ethz.ch>
2022-04-06 11:07:53 +0200

    change naming variables

--- climada_petals/engine/warn.py
+++ climada_petals/engine/warn.py
@@ -1,8 +1,6 @@
 import logging
 import numpy as np
-import skimage.morphology as morph
-import skimage.filters as filters
-from skimage.measure import label
+import skimage
 import copy
 from dataclasses import dataclass
 
@@ -36,8 +34,8 @@ class Warn:
     def from_np_array(cls, data, thresholds, expand=True, operations=operations, sizes=sizes):  # pass defaults
         filter_data = FilteringOrder(thresholds, expand, operations, sizes)
         warn = cls(filter_data, data)
-        d_thrs = warn.threshold_data(data)
-        warn.filter_algorithm(d_thrs)
+        data_thrs = warn.threshold_data(data)
+        warn.filter_algorithm(data_thrs)
         return warn
 
     def threshold_data(self, data):
@@ -50,21 +48,23 @@ class Warn:
         return m_thrs.astype(int)
 
     def filter_algorithm(self, d_thrs):
-        def filtering(d_thrs, warn_reg, curr_lvl):
+        def filtering(d, warn_reg, curr_lvl):
             if self.filter_data.expand:  # select points where level is >= level under observation -> expands regions
-                pts_curr_lvl = np.bitwise_or(warn_reg, d_thrs >= curr_lvl)
+                pts_curr_lvl = np.bitwise_or(warn_reg, d >= curr_lvl)
             else:  # select points where level is == level under observation -> not expanding regions
-                pts_curr_lvl = d_thrs == curr_lvl
+                pts_curr_lvl = d == curr_lvl
             reg_curr_lvl = np.where(pts_curr_lvl, curr_lvl, 0)  # set everything but pts of current level to 0
 
             for i in range(len(self.filter_data.operations)):
                 if self.filter_data.operations[i] == 'DILATION':
-                    reg_curr_lvl = morph.dilation(reg_curr_lvl, morph.disk(self.filter_data.sizes[i]))
+                    reg_curr_lvl = skimage.morphology.dilation(reg_curr_lvl,
+                                                               skimage.morphology.disk(self.filter_data.sizes[i]))
                 elif self.filter_data.operations[i] == 'EROSION':
-                    reg_curr_lvl = morph.erosion(reg_curr_lvl, morph.disk(self.filter_data.sizes[i]))
+                    reg_curr_lvl = skimage.morphology.erosion(reg_curr_lvl,
+                                                              skimage.morphology.disk(self.filter_data.sizes[i]))
                 elif self.filter_data.operations[i] == 'MEDIANFILTERING':
-                    filter = np.ones((self.filter_data.sizes[i], self.filter_data.sizes[i]))
-                    reg_curr_lvl = filters.median(reg_curr_lvl, filter)
+                    filter_med = np.ones((self.filter_data.sizes[i], self.filter_data.sizes[i]))
+                    reg_curr_lvl = skimage.filters.median(reg_curr_lvl, filter_med)
                 else:
                     LOGGER.warning("The operation is not defined. "
                                    "Please select 'EROSION', 'DILATION', or 'MEDIANFILTERING'.")
@@ -74,39 +74,39 @@ class Warn:
         if max_warn_level == 0:
             return np.zeros(d_thrs.shape)
 
-        warn_reg = 0
+        warn_regions = 0
         cnt = 0
         # iterate from highest level to lowest (0 not necessary, because rest is level 0)
-        for i in range(max_warn_level, 0, -1):
-            w_l = filtering(d_thrs, warn_reg, i)
+        for j in range(max_warn_level, 0, -1):
+            w_l = filtering(d_thrs, warn_regions, j)
             # keep warn regions of higher levels by taking maximum
-            warn_reg = np.maximum(warn_reg, w_l)
+            warn_regions = np.maximum(warn_regions, w_l)
             cnt += 4
-        self.warning = warn_reg
+        self.warning = warn_regions
 
     @staticmethod
     def remove_small_regions(warning, size_thr):
 
-        def increase_levels(warning, size_thr):
+        def increase_levels(warn, size):
             # increase levels of too small regions to max level of this warning
-            labels = label(warning)
+            labels = skimage.measure.label(warn)
             for i in range(np.max(labels)):
                 cnt = np.count_nonzero(labels == i)
-                if cnt <= size_thr:
-                    warning[labels == i] = np.max(warning, axis=(0, 1))
-            return warning
+                if cnt <= size:
+                    warn[labels == i] = np.max(warn, axis=(0, 1))
+            return warn
 
-        def set_new_lvl(warning, size_thr):
+        def set_new_lvl(warn, size):
             # correct the max_lvl regions generated before down,
             # until the new regions with it are large enough
-            for i in range(np.max(warning, axis=(0, 1)), np.min(warning, axis=(0, 1)), -1):
-                level = copy.deepcopy(warning)
-                level[warning != i] = 0
-                labels = label(warning)
+            for i in range(np.max(warn, axis=(0, 1)), np.min(warn, axis=(0, 1)), -1):
+                level = copy.deepcopy(warn)
+                level[warn != i] = 0
+                labels = skimage.measure.label(warn)
                 for j in range(len(np.unique(labels)) + 1):
                     cnt = np.count_nonzero(labels == j)
-                    if cnt <= size_thr:
-                        warning[labels == j] = i - 1
+                    if cnt <= size:
+                        warn[labels == j] = i - 1
 
         warning = warning + 1  # 0 is regarded as background in labelling, + 1 prevents this
         increase_levels(warning, size_thr)
