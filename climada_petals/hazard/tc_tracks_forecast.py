ea718616a65a709f8ca6e739db1d7ac767b66e23
Alessio Ciullo <29018864+aleeciu@users.noreply.github.com>
2022-05-20 13:48:46 +0200

    fix typo

--- climada_petals/hazard/tc_tracks_forecast.py
+++ climada_petals/hazard/tc_tracks_forecast.py
@@ -86,7 +86,7 @@ MISSING_DOUBLE = ec.CODES_MISSING_DOUBLE
 MISSING_LONG = ec.CODES_MISSING_LONG
 """Missing double and integers in ecCodes """
 
-CXML2CSV_XSL = Path(__file__).parent / "tc_tracks_foreast_cxml2csv.xsl"
+CXML2CSV_XSL = Path(__file__).parent / "tc_tracks_forecast_cxml2csv.xsl"
 """Path at which an xsl is found for transforming CXML to CSV format."""
 
 BASIN_ENV_PRESSURE_CXML = {
