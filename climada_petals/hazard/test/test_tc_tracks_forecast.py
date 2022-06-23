e8bab13210c2412b22ade1c9d5770f6b356f99fb
Alessio Ciullo <29018864+aleeciu@users.noreply.github.com>
2022-06-23 16:54:06 +0200

    fix tests failure

--- climada_petals/hazard/test/test_tc_tracks_forecast.py
+++ climada_petals/hazard/test/test_tc_tracks_forecast.py
@@ -153,10 +153,10 @@ class TestCXML(unittest.TestCase):
             )
         )
         self.assertEqual(forecast.data[0].name, "Vernon")
-        self.assertTrue(all([i.ensemble_number == 24 for i in forecast.data]))
+        self.assertTrue(all([i.ensemble_number == '24' for i in forecast.data]))
         self.assertEqual(
             forecast.data[2].run_datetime,
-            pd.Timestamp("2022-03-02 12:00:00+0000", tz="UTC"),
+            pd.Timestamp("2022-03-02 12:00:00"),
         )
         self.assertTrue(forecast.data[4].is_ensemble)
 
