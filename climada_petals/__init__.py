f42053bc2bdfe8ab889575b3809b8af6f0da14af
emanuel-schmid <schmide@ethz.ch>
2023-08-17 16:39:01 +0200

    data: move all files required for climada_petals package to climada_petals/data

--- climada_petals/__init__.py
+++ climada_petals/__init__.py
@@ -26,23 +26,28 @@ import climada_petals.util.config
 from .util.constants import *
 
 REPO_DATA = {
-    'data/system': [
+    'climada_petals/data/gdp_asset': [
         SYSTEM_DIR.joinpath('GDP2Asset_converter_2.5arcmin.nc'),
+        DEMO_GDP2ASSET,
     ],
-    'data/demo': [
+    'climada_petals/data/river_flood': [
         HAZ_DEMO_FLDDPH,
         HAZ_DEMO_FLDFRC,
-        DEMO_GDP2ASSET,
+    ],
+    'climada_petals/data/crop_production': [
         DEMO_DIR.joinpath('crop_production_demo_data_yields_CHE.nc4'),
         DEMO_DIR.joinpath('crop_production_demo_data_cultivated_area_CHE.nc4'),
         DEMO_DIR.joinpath('FAOSTAT_data_producer_prices.csv'),
         DEMO_DIR.joinpath('FAOSTAT_data_production_quantity.csv'),
-        DEMO_DIR.joinpath('gepic_gfdl-esm2m_ewembi_historical_2005soc_co2_yield-whe-noirr_global_DEMO_TJANJIN_annual_1861_2005.nc'),
         DEMO_DIR.joinpath('hist_mean_mai-firr_1976-2005_DE_FR.hdf5'),
         DEMO_DIR.joinpath('histsoc_landuse-15crops_annual_FR_DE_DEMO_2001_2005.nc'),
-        DEMO_DIR.joinpath('lpjml_ipsl-cm5a-lr_ewembi_historical_2005soc_co2_yield-whe-noirr_annual_FR_DE_DEMO_1861_2005.nc'),
+    ],
+    'climada_petals/data/relative_cropyield': [
+        DEMO_DIR.joinpath('gepic_gfdl-esm2m_ewembi_historical_2005soc_co2_yield-whe-noirr_global_DEMO_TJANJIN_annual_1861_2005.nc'),
         DEMO_DIR.joinpath('pepic_miroc5_ewembi_historical_2005soc_co2_yield-whe-firr_global_annual_DEMO_TJANJIN_1861_2005.nc'),
         DEMO_DIR.joinpath('pepic_miroc5_ewembi_historical_2005soc_co2_yield-whe-noirr_global_annual_DEMO_TJANJIN_1861_2005.nc'),
+    ],
+    'climada_petals/data/wildfire': [
         DEMO_DIR.joinpath('Portugal_firms_June_2017.csv'),
         DEMO_DIR.joinpath('Portugal_firms_2016_17_18_MODIS.csv'),
     ]
