340de9d04a238f80b2ab704b3fddc22679ac0794
emanuel-schmid <schmide@ethz.ch>
2023-09-07 14:49:51 +0200

    change versioning of develop branch: increase patch number by one

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(here / 'doc/misc/README.md', encoding='utf-8') as f:
 setup(
     name='climada_petals',
 
-    version='4.0.1-dev',
+    version='4.0.2-dev',
 
     description='CLIMADA Extensions in Python',
 
