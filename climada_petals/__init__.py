94459da6701999f170bb2baa588162575b73c506
Thomas Vogt <thomas.vogt@pik-potsdam.de>
2023-10-10 13:05:16 +0200

    tc_rainfield: include files in REPO_DATA

--- climada_petals/__init__.py
+++ climada_petals/__init__.py
@@ -51,7 +51,12 @@ REPO_DATA = {
     'climada_petals/data/wildfire': [
         DEMO_DIR.joinpath('Portugal_firms_June_2017.csv'),
         DEMO_DIR.joinpath('Portugal_firms_2016_17_18_MODIS.csv'),
-    ]
+    ],
+    'climada_petals/data/tc_rainfield': [
+        SYSTEM_DIR.joinpath('topography_land_360as.tif'),
+        SYSTEM_DIR.joinpath('c_drag_500.tif'),
+        DEMO_DIR.joinpath('tcrain_examples.nc'),
+    ],
 }
 
 
