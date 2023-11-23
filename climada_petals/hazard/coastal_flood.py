94a200462d10da31c4f6acca1a4b22a1289471f6
Alessio Ciullo <29018864+aleeciu@users.noreply.github.com>
2023-11-23 11:26:53 +0100

    complete docstring; use config; clean function

--- climada_petals/hazard/coastal_flood.py
+++ climada_petals/hazard/coastal_flood.py
@@ -1,14 +1,14 @@
+from typing import Iterable, Union, Optional
 import numpy as np
-import geopandas as gpd
-from pathlib import Path
 from shapely.geometry import Polygon
 
 from climada.hazard import Hazard
 from climada.util import files_handler as u_fh
 import climada.util.coordinates as u_coord
+from climada import CONFIG
 
-SOURCE_LINK = 'http://wri-projects.s3.amazonaws.com/AqueductFloodTool/download/v2/'
-DOWNLOAD_DIRECTORY = Path.cwd() # TODO: we'll rather need something like CONFIG.hazard.coastal_flood.dir()
+AQUEDUCT_SOURCE_LINK = CONFIG.hazard.coastal_flood.resources.aqueduct.str()
+DOWNLOAD_DIRECTORY = CONFIG.hazard.coastal_flood.local_data.aqueduct.dir()
 
 HAZ_TYPE = 'CF'
 """Hazard type acronym CoastalFlood"""
@@ -16,9 +16,9 @@ HAZ_TYPE = 'CF'
 __all__ = ['CoastalFlood']
 
 class CoastalFlood(Hazard):
-    """Contains coastal flood events pulled by
-    the Acqueduct project :
-    https://www.wri.org/data/aqueduct-floods-hazard-maps
+    """
+    Contains coastal flood events pulled by the Acqueduct project :
+        https://www.wri.org/data/aqueduct-floods-hazard-maps
     """
 
     def __init__(self, **kwargs):
@@ -27,112 +27,100 @@ class CoastalFlood(Hazard):
         Hazard.__init__(self, HAZ_TYPE, **kwargs)
 
     @classmethod
-    def from_aqueduct_tif(cls, rcps=None, years=None, return_periods=None,
-                           subsidence=['nosub'], projection=['0'],
-                           centroids=None, countries=None, shape=None,
-                           boundaries=None, dwd_dir=DOWNLOAD_DIRECTORY):
+    def from_aqueduct_tif(cls,
+                          rcp: str,
+                          target_year : int,
+                          return_periods: Union[str, Iterable[str]],
+                          subsidence: Optional[str] = 'wtsub',
+                          percentile: str = '95',
+                          countries: Optional[Union[str, Iterable[str]]] = None,
+                          boundaries: Iterable[float] = None,
+                          dwd_dir=DOWNLOAD_DIRECTORY):
         """
-        TODO: proper docstring
+        Download and load coastal flooding events from the Aqueduct dataset.
+        For more info on the data see: 
+            https://files.wri.org/d8/s3fs-public/aqueduct-floods-methodology.pdf
 
-        rcps : list or str
-            RCPs scenarios. Possible values are ...
-        years : list, str or int
-            simulations' reference years. Possible values are ...
+        Parameters
+        ----------
+        rcp : str
+            RCPs scenario. Possible values are historical, 4.5 and 8.5.
+        target_year : int
+            future target year. Possible values are hist, 2030, 2050, 2080.
         return_periods : list or str
-            simulations' return periods. Possible values are ...
-        subsidence : list or str
-            If there land subsidence is simulated or not. 
-            Possible values are 'nosub' and 'wtsub'.
-        projection : list or str
-            TODO: investigate meaning.
-            EXPLAIN HERE. Possible values are ... 
-        centroids : Centroids
-            centroids to extract
-        countries : list of countries ISO3
-            selection of countries
-        shape : 
+            events' return periods.
+            Possible values are 2, 5, 10, 25, 50, 100, 250, 500, 1000.
+        subsidence : str
+            If land subsidence is simulated or not.
+            Possible values are nosub and wtsub. Default wtsub.
+        percentile : str
+            Sea level rise scenario (in percentile).
+            Possible values are 5, 50 and 95.
+        countries : str or list of str
+            countries ISO3 codes
+        boundaries : tuple of floats
+            geographical boundaries in the order:
+                minimum longitude, minimum latitude,
+                maximum longitude, maximum latitude
 
+        Returns
+        -------
+        haz : CoastalFlood instance
         """
 
-        # assess number of files and from there the event_ids, these should be then passed one
-        # by one as attrs in haz.from_raster, otherwise it wont concat properly
+        if (target_year == 'hist') & (rcp != 'historical'):
+            raise ValueError(f"RCP{rcp} cannot have hist as target year")
 
-        if isinstance(rcps, str):
-            rcps = [rcps]
-        if isinstance(years, (str, int)):
-            years = [years]
+        if (rcp == 'historical') & (subsidence == 'nosub') & (target_year != 'hist'):
+            raise ValueError("Historical without subsidence can only have hist as target year")
+
         if isinstance(return_periods, str):
             return_periods = [return_periods]
-        if isinstance(subsidence, str):
-            subsidence = [subsidence]
-        if isinstance(projection, str):
-            projection = [projection]
+
+        if isinstance(countries, str):
+            countries = [countries]
+
+        rcp_name = f"rcp{rcp[0]}p{rcp[1]}" if rcp in ['4.5', '8.5'] else rcp
+        rps_name = [f"{rp.zfill(4)}" for rp in return_periods]
+        perc_name = f"0_perc{percentile.zfill(2)}" if percentile in ['05', '50'] else '0'
 
         file_names = [
-                f'inuncoast_{rcp}_{sub}_{year}_rp{rp.zfill(4)}_{proj}.tif'
-                    for rcp in rcps
-                    for sub in subsidence
-                    for year in years
-                    for rp in return_periods
-                    for proj in projection
-                    # You can't have: 
-                    # - year historic with rcp different than historical
-                    # - rcp historical, no subsidence and year different than historic
-                    if not (
-                            ((year == 'hist') & (rcp != 'historical')) |
-                            ((rcp == 'historical') & (sub == 'nosub') & (year != 'hist'))
-                            )
+                f'inuncoast_{rcp_name}_{subsidence}_{target_year}_rp{rp.zfill(4)}_{perc_name}.tif'
+                for rp in rps_name
                     ]
 
         hazs = []
-
         for i, file_name in enumerate(file_names):
-            link_to_file = "".join([SOURCE_LINK, file_name])
-            downloaded_file = dwd_dir / file_name
+            link_to_file = "".join([AQUEDUCT_SOURCE_LINK, file_name])
+            file_path = dwd_dir / file_name
 
-            if not downloaded_file.exists():
-                u_fh.download_file(
-                link_to_file,
-                download_dir = dwd_dir,
-                )
+            if not file_path.exists():
+                u_fh.download_file(link_to_file, download_dir=dwd_dir)
 
             if countries:
                 geom = u_coord.get_land_geometry(countries).geoms
 
-            # TODO: implement shape
-            elif shape:
-                # shapes = gpd.read_file(shape) # set projection
-                # geom = shapes.geometry[0]
-                pass
-
-            # TODO: implement centroids
-            elif centroids:
-                pass
-
             elif boundaries:
                 min_lon, min_lat, max_lon, max_lat = boundaries
                 geom = [Polygon([(min_lon, min_lat),
-                                (max_lon, min_lat),
-                                (max_lon, max_lat),
-                                (min_lon, max_lat)])]
-
+                                 (max_lon, min_lat),
+                                 (max_lon, max_lat),
+                                 (min_lon, max_lat)])]
             else:
                 geom = None
-                # TODO: LOGGER(loading for the whole world)
 
-            _, rcp, sub, year, rp, proj = file_name.split('.tif')[0].split('_')
-            rp = rp.lstrip('rp0')
+            event_name = f'1-in-{return_periods[i]}y_{percentile}pct_{rcp_name}_{target_year}_{subsidence}'
 
-            haz = cls().from_raster(files_intensity=file_name,
-                                          geometry=geom,
-                                          attrs={
-                                                'event_id': np.array([i+1]),
-                                                'event_name': np.array([f'{rcp}_{sub}_{year}_{rp}rp_{proj}']),
-                                                #TODO: check if proper assignment
-                                                'frequency': np.array([1 / int(rp)])
-                                                })
+            haz = cls().from_raster(files_intensity=file_path,
+                                    geometry=geom,
+                                    attrs={'event_id': np.array([i+1]),
+                                           'event_name': np.array([event_name]),
+                                           'frequency': np.array([1 / int(return_periods[i])])})
             haz.units = 'm'
             haz.centroids.set_meta_to_lat_lon()
             hazs.append(haz)
-    
-        return cls().concat(hazs)
+
+        haz = cls().concat(hazs)
+        haz.frequency = np.diff(haz.frequency, prepend=0)
+
+        return haz
