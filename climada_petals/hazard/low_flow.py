1be80a8f427fe4998e8991ef58dae43b4f38a4f1
emanuel-schmid <schmide@ethz.ch>
2024-02-23 14:36:44 +0100

    hazard.low_flow: centroids resolution in any case determined with util.coordinates.get_resolution

--- climada_petals/hazard/low_flow.py
+++ climada_petals/hazard/low_flow.py
@@ -539,11 +539,7 @@ class LowFlow(Hazard):
         -------
         float
         """
-        if centroids.meta:
-            res_centr = abs(centroids.meta['transform'][4]), \
-                        centroids.meta['transform'][0]
-        else:
-            res_centr = np.abs(u_coord.get_resolution(centroids.lat, centroids.lon))
+        res_centr = np.abs(u_coord.get_resolution(centroids.lat, centroids.lon))
         if np.abs(res_centr[0] - res_centr[1]) > 1.0e-6:
             LOGGER.warning('Centroids do not represent regular pixels %s.', str(res_centr))
             return (res_centr[0] + res_centr[1]) / 2
