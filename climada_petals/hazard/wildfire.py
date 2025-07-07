70812332ddb7557e6e8b6075d6276110adc57e23
Carmen Steinmann <carmenst@student.ethz.ch>
2025-07-07 15:58:26 +0200

    Add resampling functions to wildfire

--- climada_petals/hazard/wildfire.py
+++ climada_petals/hazard/wildfire.py
@@ -27,10 +27,20 @@ from scipy import sparse
 import os
 import pandas as pd
 from scipy.sparse import lil_matrix
+import datetime
+import scipy as sp
+from scipy.sparse import dok_matrix
+import xarray as xr
+import calendar
 
 from climada.hazard.base import Hazard
 import climada.util.dates_times as u_dates
 from climada.util import coordinates as u_coord
+from climada.util.api_client import Client
+
+LENGTH_MODIS_TILE = 0.46331271653 #km documentation:463.31271653m
+AREA_MODIS_TILE = LENGTH_MODIS_TILE**2
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -155,8 +165,226 @@ def _get_csv_file_paths(directory):
                 csv_file_paths.append(os.path.join(root, file))
     return csv_file_paths
     
+ 
+def sum_haz_per_timestep(haz, timestep, filename=None):
+    """Aggregate in time"""
+
+    dates0 = [datetime.datetime.fromordinal(date) for date in haz.date]
+    # event_id, name und date funktioniert noch nicht!
+    if timestep == 'year':
+        dates = [datetime.date(date.year, 1, 1).toordinal() for date in dates0]
+        time_array = pd.date_range(start=str(dates0[0].year)+"-01-01", end=str(dates0[-1].year+1)+"-01-01",
+                      freq='Y')
+        event_name = time_array.strftime("%Y").tolist()
     
+    if timestep == 'season_StateOfWF':
+        # 1. Filter to only full March–Feb seasons
     
+        # Find first March or later
+        for i_start, d in enumerate(dates0):
+            if d.month == 3:
+                break
+    
+        # Find last February or earlier
+        for i_end in reversed(range(len(dates0))):
+            if dates0[i_end].month == 2:
+                break
+    
+        # Slice valid dates and intensity
+        valid_dates0 = dates0[i_start:i_end+1]
+        valid_intensity = haz.intensity[i_start:i_end+1, :]
+        valid_fraction = haz.fraction[i_start:i_end+1, :]
+    
+        # 2. Assign March–Feb season years
+        season_years = []
+        for d in valid_dates0:
+            season_year = d.year if d.month >= 3 else d.year - 1
+            season_years.append(datetime.date(season_year, 3, 1).toordinal())
+    
+        dates = season_years
+    
+        # 3. Create time steps and labels
+        start_year = valid_dates0[0].year if valid_dates0[0].month >= 3 else valid_dates0[0].year - 1
+        end_year = valid_dates0[-1].year if valid_dates0[-1].month >= 3 else valid_dates0[-1].year - 1
+    
+        feb_days = [calendar.monthrange(y + 1, 2)[1] for y in range(start_year, end_year+1)]
+        time_array = pd.to_datetime([f"{y + 1}-02-{day}" for y, day in zip(range(start_year, end_year+1), feb_days)])
+        event_name = [f"{y}-{y+1}" for y in range(start_year, end_year+1)]
+    
+        # 4. Replace haz.intensity and fraction with trimmed version for aggregation
+        haz.intensity = valid_intensity
+        haz.fraction = valid_fraction
+
+        
+    elif timestep == 'month': 
+        dates = [datetime.date(date.year, date.month, 1).toordinal() for date in dates0]
+        time_array = pd.date_range(start=str(dates0[0].year)+"-01-01", end=str(dates0[-1].year+1)+"-01-01",
+                      freq='M')
+        event_name = time_array.strftime("%Y-%m").tolist()
+        
+    
+    unique_tst, unique_idx = np.unique(dates, return_index=True)
+    all_unique_tstp = np.append(unique_idx, (len(dates)-1))
+
+    new_intensity = []
+    new_fraction = []
+    nr_tstp = len(unique_idx)
+    for idx in range(nr_tstp):
+        new_intensity.append(sp.sparse.csr_matrix(np.max(haz.intensity[
+            all_unique_tstp[idx]:all_unique_tstp[idx+1],:],axis=0)))
+        new_fraction.append(sp.sparse.csr_matrix(np.sum(haz.fraction[
+            all_unique_tstp[idx]:all_unique_tstp[idx+1],:],axis=0)))
+    
+    intensity2 = sp.sparse.vstack(new_intensity).tocsr()
+    fraction2 = sp.sparse.vstack(new_fraction).tocsr()
+    
+    haz_year = create_wf_haz(intensity2, haz.centroids, time_array, 
+                             fraction=fraction2, units='', 
+                             event_name=event_name)
+    
+    if filename is not None:
+        haz_year.write_hdf5(filename)
+    
+    return haz_year    
+ 
+
+def create_time_range(years, hemisphere):
+    
+    start_year, end_year = years
+    
+    if hemisphere == 'NH':
+        start_day = str(start_year)+'-01-01'
+        end_day = str(end_year)+'-12-31'
+    elif hemisphere == 'SH':
+        start_day = str(start_year)+'-07-01'
+        end_day = str(end_year)+'-06-30'
+    
+    timerange = [start_day, end_day]
+    
+    return timerange
+
+
+def resample_wf(haz, target_centr, units=''):
+    """Aggregate in space""" 
+    nonzero_idx = np.unique(haz.intensity.indices)
+    start_coord = haz.centroids.coord[nonzero_idx,:]
+    sub_intensity = haz.intensity[:,nonzero_idx]
+    # start_coord = haz.centroids.coord
+    # sub_intensity = haz.intensity
+    assigned_coord = u_coord.match_coordinates(start_coord, target_centr.coord)
+    
+
+    event_ids = haz.event_id-1
+    n_ev = len(event_ids)
+    n_chunks = np.round(n_ev/12).astype(int)
+    arrays = np.array_split(event_ids, n_chunks)
+    
+    new_intensity = dok_matrix((haz.intensity.shape[0], target_centr.lat.size))
+ 
+    for array_chunk in arrays:
+        pd_intensity = pd.DataFrame(sub_intensity[array_chunk,:].todense().T, columns=haz.event_id[array_chunk])
+        # pd_intensity[pd_intensity == 0] = np.nan
+        pd_intensity['assigned_coord'] = assigned_coord
+        
+        if units == 'km2':
+            pd_intensity['assigned_coord'] = assigned_coord
+            pd_agg = pd_intensity.groupby('assigned_coord').sum()*AREA_MODIS_TILE
+        elif units == 'nonzero_mean':
+            pd_intensity[pd_intensity == 0] = np.nan
+            pd_intensity['assigned_coord'] = assigned_coord
+            pd_agg = pd_intensity.groupby('assigned_coord').mean()
+        elif units == 'sum':
+            pd_intensity[pd_intensity == 0] = np.nan
+            pd_intensity['assigned_coord'] = assigned_coord
+            pd_agg = pd_intensity.groupby('assigned_coord').sum()
+        elif units == 'max':
+            pd_intensity[pd_intensity == 0] = np.nan
+            pd_intensity['assigned_coord'] = assigned_coord
+            pd_agg = pd_intensity.groupby('assigned_coord').max()
+        elif units == '%':
+            pd_intensity['assigned_coord'] = assigned_coord
+            pd_agg = pd_intensity.groupby('assigned_coord').mean()*100
+    
+        new_intensity[array_chunk[0]:array_chunk[-1]+1, pd_agg.index.values] = pd_agg.values.T
+    
+    #add removing nan values from sparse matrix
+        
+    new_intensity2 = new_intensity.tocsr()
+    
+    haz_new_coord = create_wf_haz(new_intensity2, target_centr, haz.event_name, units)
+    
+    return haz_new_coord
+
+def resample_to_fraction(haz, target_centr, threshold, nr_chunks=200):
+    """Aggregate in space""" 
+    #threshold in km
+    
+    total_centroids, _ = haz.centroids.coord.shape
+    n_cent_chunk = np.round(total_centroids/nr_chunks).astype(int)
+    
+    nr_observations = lil_matrix((haz.intensity.shape[0], target_centr.lat.size))
+    # nr_cells = dok_matrix((haz.intensity.shape[0], target_centr.lat.size))
+    nr_cells = dok_matrix((1, target_centr.lat.size))
+    
+    for i in range(nr_chunks):
+        print(i, 'of', nr_chunks)
+        
+        if i <= nr_chunks-2:
+            start_coord = haz.centroids.coord[n_cent_chunk*i: n_cent_chunk*(i+1), :]
+            pd_intensity = pd.DataFrame(haz.intensity[:,n_cent_chunk*i: n_cent_chunk*(i+1)].todense().T, 
+                                        columns=haz.event_id)
+        else: 
+            start_coord = haz.centroids.coord[n_cent_chunk*i:, :]
+            pd_intensity = pd.DataFrame(haz.intensity[:,n_cent_chunk*i:].todense().T, 
+                                        columns=haz.event_id)
+        assigned_coord = u_coord.match_coordinates(start_coord, target_centr.coord, threshold=threshold)
+        pd_intensity['assigned_coord'] = assigned_coord
+        
+        pd_sum = pd_intensity.groupby('assigned_coord').sum()
+        pd_count = pd_intensity.groupby('assigned_coord').count()[1]
+        
+        try: 
+            df_sum = pd_sum.drop(-1, axis=0)
+        except KeyError:
+            df_sum = pd_sum
+        try:
+            df_count = pd_count.drop(-1, axis=0)
+        except KeyError:
+            df_count = pd_count
+        
+        nr_observations[:, df_sum.index.values] += df_sum.values.T
+        nr_cells[0, df_count.index.values] += df_count.values.T
+        
+    # Convert dok_matrix to csr_matrix
+    csr_fires = nr_observations.tocsr()
+    csr_cells = nr_cells.tocsr()
+    
+    # Perform element-wise division
+    fraction = csr_fires.multiply(csr_cells.power(-1))
+
+    haz_new_coord = create_wf_haz(fraction, target_centr, haz.event_name, units='')
+    
+    return haz_new_coord
+
+def get_CLIMADA_centr(**kwargs):
+    # lon_min, lat_min, lon_max, lat_max = extent
+    client = Client()
+    centroids = client.get_centroids(**kwargs) #extent = (lon_min, lon_max, lat_min, lat_max)
+    
+    lat_centroids, lat_idx = np.unique(centroids, return_index=True)
+    lat_centroids[np.argsort(lat_idx)]
+    lon_centroids, lon_idx = np.unique(centroids, return_index=True)
+    lon_centroids[np.argsort(lon_idx)]
+    ds_centroids = xr.Dataset(
+        {
+            "lat": (["lat"], lat_centroids, {"units": "degrees_north"}),
+            "lon": (["lon"], lon_centroids, {"units": "degrees_east"}),
+        }
+    )
+    
+    return centroids, ds_centroids
+
+   
     
     
     """VISIBLE METHODS OF DEPRECATED FORMER MODULE"""
