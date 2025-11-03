f502208f0348f7c901d22e3f1af28bf1265a20a8
KaiOBerg <bergmueller98@web.de>
2025-11-03 13:00:32 +0100

    change subarea building to resolution input

--- climada_petals/engine/cat_bonds/subareas.py
+++ climada_petals/engine/cat_bonds/subareas.py
@@ -7,10 +7,15 @@ import pandas as pd
 from shapely.ops import unary_union
 from rasterio.features import shapes, rasterize
 from rasterio.transform import from_bounds
+from sklearn.neighbors import NearestNeighbors
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 
 # specify resultion to change exposure layer into country polygons
-resolution = 1000
+#tc_bound_resolution = 1000
 
 
 class Subareas:
@@ -25,12 +30,10 @@ class Subareas:
             Vulnerability object containing vulnerability data.
         exposure : climada.Exposure
             Exposure object containing monetary data.
-        grid_specs : dict or object
-            Specifications for the spatial grid (number of rows and columns). Defines count of subaraeas.
+        resolution : float
+            Resolution for grid cells to create subareas.
         buffer_grid_size : int, optional
             Size of the buffer around input country. Resulting geometry is used to derive subareas (in km; default is 5).
-        min_pol_size : int, optional
-            Minimum polygon size for subareas (default: 1000 square meters).
         crs : str, optional
             Coordinate reference system for spatial data (default: "EPSG:3857").
         subareas_gdf : geopandas.GeoDataFrame
@@ -45,23 +48,19 @@ class Subareas:
         hazard,
         vulnerability,
         exposure,
-        grid_specs,
+        resolution,
         buffer_grid_size=5.0,
-        min_pol_size=1000,
-        crs="EPSG:3857",
     ):
 
         self.hazard = hazard
         self.vulnerability = vulnerability
         self._exposure = exposure
-        self._grid_specs = grid_specs
+        self._resolution = resolution
         self._buffer_grid_size = buffer_grid_size
-        self._min_pol_size = min_pol_size
-        self._crs = crs
         self._build_subareas()
 
     def _build_subareas(self):
-        """Recalculate subareas and islands."""
+        """Calculate subareas and islands."""
         self.subareas_gdf, self.exp_gdf = self._init_subareas()
 
     # --- Properties with auto-rebuild ---
@@ -69,46 +68,13 @@ class Subareas:
     def exposure(self):
         return self._exposure
 
-    @exposure.setter
-    def exposure(self, value):
-        self._exposure = value
-        self._build_subareas()
-
     @property
-    def grid_specs(self):
-        return self._grid_specs
-
-    @grid_specs.setter
-    def grid_specs(self, value):
-        self._grid_specs = value
-        self._build_subareas()
+    def resolution(self):
+        return self._resolution
 
     @property
     def buffer_grid_size(self):
         return self._buffer_grid_size
-
-    @buffer_grid_size.setter
-    def buffer_grid_size(self, value):
-        self._buffer_grid_size = value
-        self._build_subareas()
-
-    @property
-    def min_pol_size(self):
-        return self._min_pol_size
-
-    @min_pol_size.setter
-    def min_pol_size(self, value):
-        self._min_pol_size = value
-        self._build_subareas()
-
-    @property
-    def crs(self):
-        return self._crs
-
-    @crs.setter
-    def crs(self, value):
-        self._crs = value
-        self._build_subareas()
 
     def plot(self):
         if self.subareas_gdf is None:
@@ -162,21 +128,15 @@ class Subareas:
         exp_gdf : GeoDataFrame
             Geodataframe of the exposure perimeter.
         """
-
+        exp_crs = self._exposure.crs
         exp_gdf = self._create_exp_gdf()
+        logging.info("Number of polygons in exposure perimeter: %d", len(exp_gdf))
         exp_gdf = exp_gdf.explode(ignore_index=True, index_parts=True)
-        buffered_geometries = exp_gdf.geometry.buffer(self._buffer_grid_size * 1000)
-        exp_gdf = unary_union(buffered_geometries)
-        exp_gdf = gpd.GeoDataFrame({"geometry": [exp_gdf]}, crs=self._crs).explode(
-            index_parts=True
-        )
+
         subareas_gdf = self._crop_grid_cells_to_polygon(
             exp_gdf
         )
-        if self._crs == "EPSG:3857":
-            exposure_crs = self._exposure.crs
-            exp_gdf = exp_gdf.to_crs(exposure_crs)
-            subareas_gdf = subareas_gdf.to_crs(exposure_crs)
+
         subareas_gdf["subarea_letter"] = [chr(65 + i) for i in range(len(subareas_gdf))]
 
         return subareas_gdf, exp_gdf
@@ -203,38 +163,51 @@ class Subareas:
             GeoDataFrame containing the cropped grid cells for all polygons, with empty geometries removed.
         """
 
+        LOGGER.info("Creating subareas from exposure perimeter polygon.")
         cropped_cells = []
+
+        LOGGER.info(f"Number of polygons to process: {len(exp_gdf)}")
 
         # Loop through each polygon in the GeoDataFrame
         for idx, polygon in exp_gdf.iterrows():
-            polygon_area_km2 = polygon.geometry.area / 1e6
-            if polygon_area_km2 < self._min_pol_size:
-                grid_gdf = gpd.GeoDataFrame(
-                    {"geometry": [polygon.geometry]}, crs=exp_gdf.crs
-                )
-                cropped_cells.append(grid_gdf)
-            else:
-                minx, miny, maxx, maxy = polygon.geometry.bounds
+            
+            minx, miny, maxx, maxy = polygon.geometry.bounds
 
-                num_cells_x = self._grid_specs[0]
-                num_cells_y = self._grid_specs[1]
-                x_coords = np.linspace(minx, maxx, num_cells_x + 1)
-                y_coords = np.linspace(miny, maxy, num_cells_y + 1)
+            LOGGER.info(
+                f"Processing polygon with bounds: {minx}, {miny}, {maxx}, {maxy}"
+            )
 
-                grid_cells = []
-                for i in range(num_cells_x):
-                    for j in range(num_cells_y):
-                        grid_cell = box(
-                            x_coords[i], y_coords[j], x_coords[i + 1], y_coords[j + 1]
-                        )
-                        cell_cropped = grid_cell.intersection(polygon.geometry)
-                        grid_cells.append(cell_cropped)
+            num_cells_x = int((maxx - minx) / self._resolution) + 1
+            num_cells_y = int((maxy - miny) / self._resolution) + 1
+            n_cols = int(np.ceil((maxx - minx) / self._resolution))
+            n_rows = int(np.ceil((maxy - miny) / self._resolution))
 
-                grid_gdf = gpd.GeoDataFrame(
-                    grid_cells, columns=["geometry"], crs=exp_gdf.crs
-                )
+            LOGGER.info(
+                f"Number of cells in x direction: {num_cells_x}, y direction: {num_cells_y}"
+            )   
 
-                cropped_cells.append(grid_gdf)
+            grid_cells = []
+            for x in range(n_cols):
+                for y in range(n_rows):
+
+                    x1 = minx + x * self._resolution
+                    y1 = miny + y * self._resolution
+                    x2 = x1 + self._resolution
+                    y2 = y1 + self._resolution
+
+                    grid_cell = box(
+                        x1, y1, x2, y2
+                    )
+
+                    if grid_cell.intersects(polygon.geometry):
+                        #cell_cropped = grid_cell.intersection(polygon.geometry)
+                        grid_cells.append(grid_cell)
+
+            grid_gdf = gpd.GeoDataFrame(
+                grid_cells, columns=["geometry"], crs=exp_gdf.crs
+            )
+
+            cropped_cells.append(grid_gdf)
 
         grids = gpd.GeoDataFrame(
             pd.concat(cropped_cells, ignore_index=True), crs=exp_gdf.crs
@@ -242,6 +215,8 @@ class Subareas:
         grids.reset_index(drop=True, inplace=True)
         subareas = grids[~grids.is_empty]
         subareas = subareas.reset_index(drop=True)
+
+        LOGGER.info("Subareas created.")
 
         return subareas
 
@@ -264,17 +239,26 @@ class Subareas:
             A GeoDataFrame containing a single merged polygon geometry representing the geometric extent of
             the country in the specified CRS.
         """
+        LOGGER.info("Creating exposure perimeter polygon from exposure data.")
+        exp_gdf = self._exposure.gdf
+        minx, miny, maxx, maxy = exp_gdf.total_bounds
 
-        exp_crs = self._exposure.gdf.to_crs(self._crs)
-        minx, miny, maxx, maxy = exp_crs.total_bounds
+        LOGGER.info(f"Exposure total bounds: {minx}, {miny}, {maxx}, {maxy}")
+        coords = np.vstack((exp_gdf.geometry.x, exp_gdf.geometry.y)).T
+        nbrs = NearestNeighbors(n_neighbors=2).fit(coords)
+        distances, _ = nbrs.kneighbors(coords)
+        res = distances[:, 1].mean() * 1.2
 
-        width = int((maxx - minx) / resolution)
-        height = int((maxy - miny) / resolution)
+        LOGGER.info(f"Approximate resolution: {res} CRS units")
 
+        width = max(int((maxx - minx) / res), 1)
+        height = max(int((maxy - miny) / res),1)
+        LOGGER.info(f"Rasterizing exposure with width: {width}, height: {height}")
+        
         transform = from_bounds(minx, miny, maxx, maxy, width, height)
 
         shapes_gen = (
-            (geom, value) for geom, value in zip(exp_crs.geometry, exp_crs["value"])
+            (geom, value) for geom, value in zip(exp_gdf.geometry, exp_gdf["value"])
         )
 
         raster = rasterize(
@@ -284,11 +268,14 @@ class Subareas:
             fill=0,
             dtype="float32",
         )
+
         mask = raster > 0
         shapes_gen = list(shapes(raster, mask=mask, transform=transform))
         polygons = [shape(geom) for geom, value in shapes_gen if value > 0]
-        exp_gdf_sep = gpd.GeoDataFrame(geometry=polygons, crs=self._crs)
+        exp_gdf_sep = gpd.GeoDataFrame(geometry=polygons, crs=exp_gdf.crs)
         merged_exp_gdf_sep = unary_union(exp_gdf_sep.geometry)
-        exp_gdf = gpd.GeoDataFrame(geometry=[merged_exp_gdf_sep], crs=self._crs)
+        exp_gdf = gpd.GeoDataFrame(geometry=[merged_exp_gdf_sep], crs=exp_gdf.crs)
+        LOGGER.info("Exposure perimeter polygon created.")
+        exp_gdf.plot()
 
         return exp_gdf
