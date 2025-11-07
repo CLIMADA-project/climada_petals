3d59693576757231cf821d49fdaf54fa84f7e0cd
KaiOBerg <bergmueller98@web.de>
2025-11-07 13:57:03 +0100

    update function description

--- climada_petals/engine/cat_bonds/bond_simulation.py
+++ climada_petals/engine/cat_bonds/bond_simulation.py
@@ -18,26 +18,26 @@ class bond_simulation:
         """
         Calculates the expected losses for a catastrophe bond over its term.
         This function simulates the bond's loss experience given a sequence of event data per year,
-        tracking payouts, remaining nominal value, and the timing of losses. It returns the relative
-        losses per year, the total relative loss, and a DataFrame detailing losses and their corresponding months.
+        tracking payouts, damages, remaining princpal value, and the timing of losses. It returns the relative
+        losses per year, the total payouts and damages per term, and a DataFrame detailing losses and their corresponding months.
         Parameters
         ----------
-        term : int
-            The term of the bond in years.
+        self : bond_simulation
+            An instance of the bond_simulation class containing a payout vs damage table, bond term, and the principal.
         events_per_year : list of pandas.DataFrame
             A list where each element is a DataFrame representing events in a year. Each DataFrame must
             contain at least 'month' and 'pay' columns, where 'pay' is the payout for each event.
-        principal : float
-            The initial principal value of the bond.
         Returns
         -------
         rel_annual_losses : numpy.ndarray
             Array of relative payouts/losses per year (losses divided by principal).
-        term_loss : float
-            Total payout/investor loss over the bond's term.
         rel_monthly_loss : pandas.DataFrame
             DataFrame with columns 'losses' and 'months', detailing the losses and their corresponding
             months for each year.
+        summed_payouts : float
+            The total summed payouts over the bond's term.
+        summed_damages : float
+            The total summed damages over the bond's term.
         """
 
         losses = []
@@ -82,21 +82,18 @@ class bond_simulation:
     '''Loop over all terms of bond to derive losses'''
     def init_exp_loss_att_prob_simulation(self):
         """
-        Simulates expected annual loss and attachment probability for a catastrophe bond over multiple years.
+        Simulates the bonds monthly losses, total payouts and damages, expected annual loss, attachment probability, and other metrics for a catastrophe bond over multiple years.
         This function processes a DataFrame of payout and damage events, simulates bond losses over a specified term,
         and computes risk metrics including Value-at-Risk (VaR) and Expected Shortfall (ES) at 95% and 99% confidence levels.
-        It returns the expected annual loss, attachment probability, a DataFrame of monthly losses, and a dictionary of risk metrics.
+        It returns the a DataFrame of monthly losses, and a dictionary of bond metrics.
         Parameters
         ----------
-            pay_vs_dam (pd.DataFrame): DataFrame containing payout and damage event data.
-            nominal (float): The nominal value of the bond.
-            print_prob (bool, optional): If True, prints the expected loss and attachment probability. Defaults to True.
+            self: bond_simulation
+                An instance of the bond_simulation class containing a payout vs damage table, bond term, and number of simulated years.
         Returns
         -------
-            exp_loss_ann (float): Expected annual loss.
-            att_prob (float): Annual attachment probability (probability that the bond is triggered).
             df_loss_month (pd.DataFrame): DataFrame containing monthly loss data for all simulations.
-            es_metrics (dict): Dictionary containing VaR and ES metrics at 95% and 99% confidence levels for annual and total losses.
+            metrics (dict): Dictionary containing expected loss, attachment probability, total payouts/damages, VaR and ES metrics at 95% and 99% confidence levels for annual losses.
         """
 
         annual_losses = []
